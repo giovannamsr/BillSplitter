ba1b4611f4048bf764fb1977ce580b9849ebf08c
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-09-17 15:51:24 -0300

    Calculating total per person

--- BillSplitter/ Controller/CalculatorViewController.swift
+++ BillSplitter/ Controller/CalculatorViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
-    @IBOutlet weak var billTotalLabel: UILabel!
     @IBOutlet weak var zeroTipButton: UIButton!
     @IBOutlet weak var tenTipButton: UIButton!
     @IBOutlet weak var twentyTipButton: UIButton!
     @IBOutlet weak var splitLabel: UILabel!
     @IBOutlet weak var calculateButton: UIButton!
-    let splitValue = 0
+    @IBOutlet weak var billTotalText: UITextField!
+    
+    var splitValue = 1
+    var totalPerPerson : Float = 0.0
+    var billTotal : Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,22 +31,25 @@ class CalculatorViewController: UIViewController {
         tenTipButton.isSelected = false
         twentyTipButton.isSelected = false
         sender.isSelected = true
+        billTotalText.endEditing(true)
         
     }
     
     @IBAction func stepperPressed(_ sender: UIStepper) {
-        splitLabel.text = String(splitValue + Int(sender.value))
+        splitLabel.text = String(Int(sender.value))
+        splitValue = Int(sender.value)
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
-        if zeroTipButton.isSelected == true{
-            print(0.0)
+        billTotal = Float(billTotalText.text ?? "0.0") ?? 0.0
+        if tenTipButton.isSelected == true{
+            billTotal = billTotal + 0.1*billTotal
         }
-        else if tenTipButton.isSelected == true{
-            print(0.1)
+        else if twentyTipButton.isSelected == true{
+            billTotal = billTotal + 0.2*billTotal
+    
         }
-        else{
-            print(0.2)
-        }
+        totalPerPerson = billTotal/Float(splitValue)
+        print(totalPerPerson)
     
     }
 }
